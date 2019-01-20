65862686098af91e29c9145991d1347a9151d6b0
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-01-20 17:05:22 +0530

    Create Sumkn.c

---
+++ Sumkn.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int sum=0,n,k,a[15];
+scanf("%d",&n); 
+scanf("%d",&k); 
+while(i<=n) 
+{
+scanf("%d",&a[i]);
+}
+while(i<=k) 
+{
+sum=sum+a[i];
+}
+printf("%d",sum); 
+}
