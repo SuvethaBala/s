44712021fb149a147a17712413c9ca16c9b3250a
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-01-31 23:53:59 -0800

    Update arms.c

--- arms.c
+++ arms.c
@@ -2,7 +2,6 @@
 void main()
 {
 int n,n1,temp=0,c;
-printf("enter the number");
 scanf("%d",&n);
 c=n;
 while(n>0)
@@ -11,7 +10,6 @@ n1=n%10;
 temp=temp+(n1*n1*n1);
 n=n/10;
 }
-printf("%d",temp);
 if(temp==c)
 {
 printf("yes");
