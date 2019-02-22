92c04766f2339a8d728d925cb1d9944d0253fe90
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-02-22 20:43:50 +0530

    Create Matchindex.c

---
+++ Matchindex.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main()
+{
+long int arr[100000],n,i;
+int f=0;
+scanf("%ld",&n);
+for(i=0;i<n;i++)
+{
+scanf("%ld",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+if(arr[i]==i)
+{
+f=1;
+printf("%d",i);
+}
+}
+if(f==0)
+{
+printf("-1");
+}
+}
