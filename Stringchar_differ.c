3a481b71544fb70150aa75bf06a37d488dd81dcb
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-03-24 11:42:30 +0530

    Update Stringchar_differ.c

--- Stringchar_differ.c
+++ Stringchar_differ.c
@@ -3,15 +3,15 @@
 int main(void)
 {
 char str1[50],str2[50];
-int i,jn1,n2,count=0;
+int i,j,n1,n2,count=0;
 scanf("%s %s",str1,str2);
 n1=strlen(str1);
 n2=strlen(str2);
-if(n=n2)
+if(n1==n2)
 {
 for(i=0:i<n1;i++)
 {
-if(str1[i]==str2[i]
+if(str1[i]==str2[i])
 {
 count=count+0;
 }
