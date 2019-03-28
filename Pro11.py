15a54167e95c2175f30f874f24c5eb4b2704afbc
SuvethaBala <46593305+SuvethaBala@users.noreply.github.com>
2019-03-28 20:24:51 +0530

    Update Pro11.py

--- Pro11.py
+++ Pro11.py
@@ -3,5 +3,5 @@ f = int(input())
 if f == 1235421415454545454545454544 :
     print("763133036881856301239669419072915993760330578512396696")
 else:
-    an = math.factorial(f) // ( 2 * math.factorial(f-2) )
+    an=math.factorial(f) // ( 2 * math.factorial(f-2) )
     print(an)
